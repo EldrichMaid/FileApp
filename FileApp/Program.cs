4a4f7e78ce98387c44acdfb37424f818c7541119
EldrichMaid <kupo@mail.ru>
2024-09-16 14:50:50 +0300

    8.1.5

--- FileApp/Program.cs
+++ FileApp/Program.cs
@@ -12,8 +12,19 @@
             FreeSpace = freespace;
         }        
     }
-    internal class Program
+
+    public class Folder
     {
+        public List<string> Files { get; set; } = new List<string>();
+    }
+    internal class Program
+    {   
+        Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+        public void CreateFolder(string name)
+        {
+            Folders.Add(name, new Folder());
+        }
+
         static void Main()
         {
             
