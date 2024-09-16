687151acb59ea9e3e15f1793cc341b08068dff68
EldrichMaid <kupo@mail.ru>
2024-09-16 14:46:49 +0300

    8.1.4

--- FileApp/Program.cs
+++ FileApp/Program.cs
@@ -1,5 +1,17 @@
 ﻿namespace FileApp
 {
+    public class Drive
+    {
+        public string Name { get; }
+        public long Space { get; }
+        public long FreeSpace { get; }
+        public Drive(string name, long space, long freespace)
+        {
+            Name = name;
+            Space = space;
+            FreeSpace = freespace;
+        }        
+    }
     internal class Program
     {
         static void Main()
