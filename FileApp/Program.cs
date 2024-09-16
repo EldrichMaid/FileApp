51218a0ecb9aa617795534fa4ba0f4e2032eb1c6
EldrichMaid <kupo@mail.ru>
2024-09-16 15:32:29 +0300

    8.2.2

--- FileApp/Program.cs
+++ FileApp/Program.cs
@@ -73,6 +73,10 @@ namespace FileApp
                 if (dirInfo.Exists)
                 {
                     Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+                    DirectoryInfo newDirectory = new DirectoryInfo(@"/newDirectory");
+                    if (!newDirectory.Exists)
+                        newDirectory.Create();
+                    Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
                 }
             }
             catch (Exception e)
